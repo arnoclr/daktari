7e5f2198820efac8b4b9ea6da33a9ffcffef608d
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-05-12 12:20:22 +0200

    ajout traitements page animal

--- pages/animals/info.php
+++ pages/animals/info.php
@@ -16,6 +16,30 @@
             <p><?= $animal->poids ?> kg</p>
             <p><?= $animal->taille_cm ?> cm</p>
         </div>
+
+        <h4>Ses rendez-vous</h4>
+        <ul class="rdvs">
+            <?php foreach($appointments as $appointment): ?>
+                <li><strong><?= $appointment->raison ?></strong>, <?= $appointment->date ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+        <h4>Ses traitements récents</h4>
+        <p>Consultez le <a href="?action=viewCalender">calendrier</a> pour plus de détails</p>
+        <div>
+            <?php foreach($traitements as $traitement): ?>
+                <?php
+                $product = $pdo->query("SELECT * FROM produits WHERE id = {$traitement->id_produit}")->fetch();
+                $card = (object) [
+                    "product" => $product,
+                    "traitement" => $traitement,
+                    "animal" => $animal
+                ];
+                include 'includes/components/cardItem.php'; 
+                ?>
+            <?php endforeach; ?>
+        </div>
+
     </section>
 
 </div>
