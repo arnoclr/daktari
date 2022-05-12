2dd36905064bcc5e211a349a5e56b8b592a43608
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-05-12 22:49:55 +0200

    fix calendrier

--- handlers/viewCalendar.php
+++ handlers/viewCalendar.php
@@ -1,6 +1,6 @@
 <?php
 
-$traitements = $pdo->query("SELECT * FROM traitements WHERE id_consultation IN (SELECT id FROM consultations WHERE id_animal IN (SELECT id FROM animaux WHERE id_proprietaire = $user->id))")->fetchAll();
+$traitements = $pdo->query("SELECT * FROM traitements WHERE id_consultation IN (SELECT id FROM consultations WHERE id_animal IN (SELECT id FROM animaux WHERE decede = FALSE AND id_proprietaire = $user->id))")->fetchAll();
 
 $traitementsPerDays = [
     [], [], []
@@ -10,9 +10,9 @@ foreach ($traitements as $traitement) {
     $startDate = $pdo->query("SELECT date FROM consultations WHERE id = $traitement->id_consultation")->fetch()->date;
     $ago = time() - strtotime($startDate);
     $daysAgo = floor($ago / (60 * 60 * 24));
-    $remaining = $traitement->duree_jours + $daysAgo;
+    $remaining = $traitement->duree_jours + $daysAgo + 1;
     $product = $pdo->query("SELECT * FROM produits WHERE id = $traitement->id_produit")->fetch();
-    $animal = $pdo->query("SELECT * FROM animaux WHERE decede = FALSE AND id = (SELECT id_animal FROM consultations WHERE id = $traitement->id_consultation)")->fetch();
+    $animal = $pdo->query("SELECT * FROM animaux WHERE id = (SELECT id_animal FROM consultations WHERE id = $traitement->id_consultation)")->fetch();
     
     for ($i = 0; $i < min(3, $remaining); $i++) {
         $traitementsPerDays[$i][] = (object) [
